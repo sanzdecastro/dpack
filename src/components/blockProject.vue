b2a3efdf3ecccbb87e340a5c611abdfba1f2daf0
sanzdecastro <sanzdecastro@gmail.com>
2025-04-04 01:22:22 +0200

    test

--- src/components/blockProject.vue
+++ src/components/blockProject.vue
@@ -36,22 +36,22 @@ export default {
 
 <template>
 
-<div class="flex flex-wrap">
-    <div class="text-block flex flex-col justify-between" :class="text.size, text.aligment">
+<div class="flex flex-wrap px-sm ">
+    <div class="text-block flex flex-col justify-between pb-sm" :class="text.size, text.aligment">
         <div class="header-block">
-            <span>{{ text.pretitle }}</span>
-            <h2>{{ text.title }}</h2>
+            <span class="font-bold text-p mb-xxs block">{{ text.pretitle }}</span>
+            <h2 class="font-display text-title">{{ text.title }}</h2>
         </div>
       
-        <p>{{ text.description }}</p>
+        <p class="text-tag mt-lg">{{ text.description }}</p>
     </div>
 
-  <div v-for="(image, index) in images.block_image" class="image-block" :class="image.size">
-    <div v-if="image.video === false" class="image-wrapper flex justify-center items-center w-full" :class="image.aspect_ratio">
+  <div v-for="(image, index) in images.block_image" class="image-block pb-sm pr-sm" :class="image.size">
+    <div v-if="image.video === false" class="image-wrapper flex justify-center items-center w-full rounded-2xl overflow-hidden" :class="image.aspect_ratio">
         <img :alt="image.image.alt" :src="image.image.url" class="w-full h-full object-cover" >
     </div>
     
-    <div v-else class="video-wrapper  flex justify-center items-center w-full h-full" :class="image.size, image.aspect_ratio">
+    <div v-else class="video-wrapper  flex justify-center items-center w-full h-full rounded-2xl overflow-hidden" :class="image.size, image.aspect_ratio">
         <video autoplay playsinline loop muted class="w-full h-full object-cover" :src="image.video.url"></video>
     </div>
   </div>
@@ -66,6 +66,9 @@ export default {
 
   
 <style>
-
+.items-center {
+    @apply
+    text-center;
+}
 </style>
   
