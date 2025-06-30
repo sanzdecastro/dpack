cfe0cea658392d00f23c847dbd8f87e82ae79312
sanzdecastro <sanzdecastro@gmail.com>
2025-06-30 16:45:40 +0200

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   integrations: [vue()],
-  output: 'static',
+  output: 'server',
   // adapter: vercel(),
   prefetch: {
     prefetchAll: true
