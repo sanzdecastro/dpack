69145f2624342545ebff6dba84c4fe4136b81877
sanzdecastro <sanzdecastro@gmail.com>
2025-12-13 15:02:30 +0100

    services

---
+++ src/components/separador.vue
@@ -0,0 +1,30 @@
+<script>
+import { gsap } from "gsap";
+export default {
+  name: "Separador",
+  props: {
+    
+  },
+  data() {
+    return {
+       
+    };
+  },
+  mounted() {
+    
+  },
+
+  methods: {
+    
+  }
+}
+</script>
+
+<template>
+    <div ref="separador" class="separador bg-primary fill-foreground ">
+      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="28" viewBox="0 0 16 28" fill="none">
+  <path d="M5.00582 28.0098L15.4533 0H10.4475L0 28.0098H5.00582Z" fill="#09090B"/>
+</svg>
+   
+</div>
+</template>
